e8a9faea41f5e4a119411fc406bff36a409b676a
csubiii <csubicsabi05@gmail.com>
2024-04-24 22:11:29 +0200

    userInfo table created

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -1,18 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsDate, IsEmail, Max, Min } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-@Entity()
+enum UserType {
+    ADMIN = "admin",
+    USER = "user"
+}
+
+@Entity({ name: "userInfo" })
 export class User {
-
     @PrimaryGeneratedColumn()
-    id: number
+    user_id: number;
 
-    @Column()
-    firstName: string
+    @Column({ type: "varchar", length: 50 })
+    ic_name: string;
 
-    @Column()
-    lastName: string
+    @Column({ type: "varchar", length: 50 })
+    steam_name: string;
 
-    @Column()
-    age: number
+    @Column({ type: "varchar", length: 30 })
+    dc_name: string;
 
+    @Column({ type: "varchar", length: 45 })
+    @IsEmail()
+    email: string;
+
+    @Column({ type: "varchar", length: 45 })
+    @Min(8)
+    @Max(45)
+    password: string;
+
+    @Column({ type: "enum", enum: UserType, default: UserType.USER })
+    userLevel: UserType;
+
+    @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
+    @IsDate()
+    joinDate: Date;
 }
