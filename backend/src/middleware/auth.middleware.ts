149fea52b60ce8a925047dbff2cfdd0c813fe377
csubiii <csubicsabi05@gmail.com>
2024-06-12 23:02:41 +0200

    user, admin authentication

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  if (req.session && req.session.username) {
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.user_id) {
     next();
   } else {
     res.status(401).send('Unauthorized');
+  }
+};
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.userLevel === 'admin') {
+    next();
+  } else {
+    res.status(403).send('Forbidden');
   }
 };
